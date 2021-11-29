f6bc6b69016ba3a49755651103bf0e03fa7ddbca
Daniel Fraile <daniel.fraile@adidas.com>
2021-11-29 11:01:29 +0100

    Modify: ensure only one instance is running

--- metrics/bctf-api.sh
+++ metrics/bctf-api.sh
@@ -4,15 +4,15 @@
 # ARGS: $@ (optional): Arguments provided to the script
 # OUTS: Variables indicating command-line parameters and options
 function parse_params() {
-    local param
+    local _param
     local _bin
 
     _bin="$(basename $0)"
 
     while [[ $# -gt 0 ]]; do
-        param="$1"
+        _param="$1"
         shift
-        case $param in
+        case $_param in
             -d | --dir)
                 # Remove slash
                 _dir="${1%/}"
@@ -22,6 +22,9 @@ function parse_params() {
                 _expires=${1}
                 shift
                 ;;
+            -f | --force)
+                _force=true
+                ;;    
             -g | --target)
                 _target=${1}
                 shift
@@ -36,10 +39,9 @@ function parse_params() {
                 ;;
             -v | --verbose)
                 _verbose=true
-                shift
                 ;;
             *)
-                echo "Invalid parameter was provided: $param"
+                echo "Invalid parameter was provided: $_param"
                 exit 1
                 ;;
         esac
@@ -74,6 +76,7 @@ Usage: bctf_metrics [args]
 Arguments:
      -d|--dir                         Cache directory path
      -e|--expires                     Curl expiry age (default 60s).
+     -f|--force                       Force cUrl request (disable caching)
      -g|--target                      Target file
      -h|--help                        Displays this help
      -t|--token                       Authorization token to use
@@ -99,43 +102,51 @@ function verbose_print() {
 function my_curl() {
     local _response
 
-    _response=$(curl -w "%{http_code}" -X ${_method} -sI --retry 10 --retry-delay 5 -H "Authorization: Bearer ${_token}" -H "api-key: ${_apikey}" ${_url};)    
+    #  make it retry ten times and sleep five seconds before retrying:--retry 10 --retry-delay 5
+    # print the http code: -w "%{http_code}"
+    _response=$(curl -w "%{http_code}" -X ${_method} -sI -H "Authorization: Bearer ${_token}" -H "api-key: ${_apikey}" ${_url};)
 
     echo "${_response}"
 }
 
 function cached_curl() {
-    local _progr
+    local _prog
     local _hash
     local _apikey
     local _url
     local _curl_response
     local _curl_send
-    
+
     _apikey=$1
     _url=$2
 
     _prog="$(basename $0)"
     _hash=$(echo "${_apikey}${_url}" | md5sum | cut -f1 -d ' ')
     _cacheFile="${_dir}/${_hash}"
+    # verbose_print "Url: ${_url}"
     verbose_print "Cache file: ${_cacheFile}"
     _tempFile=$(mktemp)
     _curl_send=false
-    if [[ -f ${_cacheFile} ]]; then
-        if [[ $(expr $(date +%s) - $(date -r "$_cacheFile" +%s)) -ge $_expires ]]; then
-            verbose_print "Cache expired, requesting to ${_url}"
-            # make it retry ten times and sleep five seconds before retrying:
-            # print the http code: -w "%{http_code}"
+    if [[ ! -d ${_dir} ]]; then
+        mkdir -p ${_dir}
+    fi
+
+    if [[ ! -z ${_force} ]]; then
+        verbose_print "Forcing request to ${_url}"
+        _curl_response="$(my_curl)"
+        _curl_send=true
+    else
+        if [[ -f ${_cacheFile} ]]; then
+            if [[ $(expr $(date +%s) - $(date -r "$_cacheFile" +%s)) -ge $_expires ]]; then
+                verbose_print "Cache expired, requesting to ${_url}"
+                _curl_response="$(my_curl)"
+                _curl_send=true
+            fi
+        else
+            verbose_print "No cache found, requesting to ${_url}"
             _curl_response="$(my_curl)"
             _curl_send=true
         fi
-    else
-        verbose_print "No cache found, requesting to ${_url}"
-        mkdir -p ${_dir}
-        #  make it retry ten times and sleep five seconds before retrying:--retry 10 --retry-delay 5
-        # print the http code: -w "%{http_code}"
-        _curl_response="$(my_curl)"
-        _curl_send=true
     fi
     # verbose_print "cUrl: ${_curl_response}"
     _curl_http_code=$(tail -n1 <<< "$_curl_response")  # get the last line
@@ -161,8 +172,22 @@ function main() {
     local _url
     local _apikey
     local _apiname
+    local _lockfile
+    local _lockfile
 
     parse_params "$@"
+    # The file which represent the lock.
+    _lockfile="$(basename $0).lock"
+    if [[ -f ${_lockfile} ]]; then
+        touch ${_lockfile}
+    fi
+    exec {_lockfd}<>${_lockfile}
+    verbose_print "Acquiring lock on ${_lockfile} (${_lockfd})"
+    if ! flock -n ${_lockfd}; then
+        verbose_print "Failed to obtain a lock "
+        verbose_print "Another instance of $(basename $0) is probably running."
+        killall -q $(basename $0)
+    fi
     override_params
     envsubst < ${_target} > /tmp/target.txt
     # Each line <apiname>,<apikey>,<url>
@@ -201,7 +226,7 @@ function main() {
             echo "${_apiname}:${_apikey}:x-ratelimit-limit-second:${_xRatelimitSecond##[[:space:]]}";
         fi;
     done < /tmp/target.txt
-    rm /tmp/target.txt
+    rm -f /tmp/target.txt ${_lockfile}
 }
 
 main "$@"
